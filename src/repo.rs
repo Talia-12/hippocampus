d9107a1f01eacff14af4cb248a0ed88a6050020e
Talia-12 <paradox.m.burgess@gmail.com>
2025-03-04 16:06:49 -0800

    Implement basic CRUD for Item

--- src/repo.rs
+++ src/repo.rs
@@ -1,13 +1,16 @@
 use crate::db::DbPool;
-use crate::models::Item;
+use crate::models::{Item, Review};
 use crate::schema::items::dsl::*;
+use crate::schema::reviews::dsl::*;
 use diesel::prelude::*;
 use anyhow::Result;
+use chrono::Utc;
+use chrono::Duration;
 
 pub fn create_item(pool: &DbPool, new_title: String) -> Result<Item> {
     let conn = &mut pool.get()?;
     let new_item = Item::new(new_title);
-    diesel::insert_into(items)
+    diesel::insert_into(crate::schema::items::table)
         .values(&new_item)
         .execute(conn)?;
     Ok(new_item)
@@ -15,8 +18,8 @@ pub fn create_item(pool: &DbPool, new_title: String) -> Result<Item> {
 
 pub fn get_item(pool: &DbPool, item_id: &str) -> Result<Option<Item>> {
     let conn = &mut pool.get()?;
-    let result = items
-        .filter(id.eq(item_id))
+    let result = crate::schema::items::table
+        .filter(crate::schema::items::id.eq(item_id))
         .first::<Item>(conn)
         .optional()?;
     Ok(result)
@@ -24,6 +27,8 @@ pub fn get_item(pool: &DbPool, item_id: &str) -> Result<Option<Item>> {
 
 pub fn list_items(pool: &DbPool) -> Result<Vec<Item>> {
     let conn = &mut pool.get()?;
-    let result = items.load::<Item>(conn)?;
+    let result = crate::schema::items::table.load::<Item>(conn)?;
     Ok(result)
+}
+
 } 
